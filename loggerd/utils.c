3cb7c9bc32f27a7c600c006ae6a6335bad44f610
siryang2006 <393121997@qq.com>
2016-06-01 19:26:32 +0800

    add remark for get_line and get_end_half_line

--- loggerd/utils.c
+++ loggerd/utils.c
@@ -2,9 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
-/**
- * @get the first line in the buffer to read,return the length of first line
- */
+/*
+*buffer:要解析的buffer
+*buffer_length:buffer的实际长度，与是否为'\0'结尾没有关系
+*next_line:获取到的行的首字符地址指针
+*remaning_length:解析完该行后剩余buffer的长度
+*is_full_line:解析出来的是否为一整行
+*return:返回解析出该行的实际字符的长度，包括'\0','\n','\r'
+*注:该函数会把buffer中的'\n','\r'替换为'\0'
+*/
 int get_line(uchar *buffer,
              int buffer_length,
              uchar **next_line,
@@ -83,7 +89,12 @@ int get_line(uchar *buffer,
 }
 
 /**
-  return:true-half line,false-full line
+  * buf_start:指向要解析buffer的第一个字符
+  * buf_end:指向要解析buffer的最后一个字符
+  * out_last_line_start:如果要解析的buffer存在半行数据，out_last_line_start指向该半行数据的起始字符的地址
+  * out_last_line_end:如果要解析的buffer存在半行数据，out_last_line_end指向该半行数据的最后一个字符的地址
+  * have_multi_lines:如果要解析的buffer存在多行，*have_multi_lines等于1，否则等于0
+  * return:true-存在半行数据，false-不存在半行数据
 */
 bool get_end_half_line(const uchar *buf_start, const uchar *buf_end,
                        uchar ** out_last_line_start, uchar **out_last_line_end,
